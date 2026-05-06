58b31d98be751e8864bc70dd0affe99af4df92da
Guna <guna.sachan@gmail.com>
2026-05-06 18:22:38 +0530

    third commit

--- src/postjiracodeprogram.java
+++ src/postjiracodeprogram.java
@@ -3,7 +3,7 @@ public class postjiracodeprogram {
 
 	public static void main(String[] args) {
 		System.out.println("the new code is adding " );	
-
+		System.out.println("the new code is adding for testing purpose " );	
 	}
 
 }
